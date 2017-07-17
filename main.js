4b3f6496ad6e80ae3a9e34dda5ea1a426c587d1b
JP Ubana <jpjubana@gmail.com>
2017-07-17 13:58:05 -0400

    Fin, All tests passed

---
+++ main.js
@@ -0,0 +1,51 @@
+//###################################################################
+//        __
+//   ____/ /___  ____ ______
+//  / __  / __ \/ __ `/ ___/
+// / /_/ / /_/ / /_/ (__  )
+// \__,_/\____/\__, /____/
+//            /____/
+
+// Dog Constructor & Prototype
+function Dog(name, color, status, hungry = true, owner) {
+    this.name = name;
+    this.color = color;
+    this.status = status;
+    this.hungry = hungry;
+    this.owner = owner;
+}
+
+
+// Instances of Dog
+// Needed: sadie, moonshine, atticus
+
+var sadie = new Dog('sadie', 'black', 'normal', false);
+var moonshine = new Dog('moonshine');
+var atticus = new Dog('atticus');
+
+//     __
+//    / /_  __  ______ ___  ____ _____  _____
+//   / __ \/ / / / __ `__ \/ __ `/ __ \/ ___/
+//  / / / / /_/ / / / / / / /_/ / / / (__  )
+// /_/ /_/\__,_/_/ /_/ /_/\__,_/_/ /_/____/
+
+// Human Constructor & Prototype
+
+function Human(name, cool = true) {
+    this.name = name;
+    this.cool = cool;
+}
+
+Human.prototype.pet = function() {
+    sadie.status = 'happy';
+}
+
+Human.prototype.feed = function() {
+    moonshine.hungry = false;
+}
+
+// Instances of Human
+// Needed: mason, julia
+
+var mason = new Human('mason', false);
+var julia = new Human('julia', true);
